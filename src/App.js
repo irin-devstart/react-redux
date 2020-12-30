6f99c9eae5932a49368958d498bba354aa53a5a4
Programmer <programmer.unknowntv@gmail.com>
2020-12-31 04:26:08 +0700

    Upload Project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import "./App.css";
+import { useSelector, useDispatch } from "react-redux";
+import { increment, decrement } from "./actions";
 function App() {
+  const counter = useSelector((state) => state.counter);
+  const isLogged = useSelector((state) => state.isLogged);
+  const dispatch = useDispatch();
+  //ACTION INCREMENT
+  //ACTION DECREMENT
+  //REDUCER
+  //untuk Set State default
+
+  //STORE -> GLOBALIZED STATE
+
+  //display it in the console
+
+  //DISPATCH
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Counter {counter}</h1>
+      {isLogged ? <h1> Sudah Login</h1> : null}
+      <button onClick={() => dispatch(increment())}>Tambah</button>
+      <button onClick={() => dispatch(decrement())}>Kurang</button>
     </div>
   );
 }
